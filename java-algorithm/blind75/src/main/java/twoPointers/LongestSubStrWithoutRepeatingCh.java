fcaeb30ccf10ccbc8da33550e2ff89c996e32b05
Trashman <jeanrevellon.dev@gmail.com>
2026-02-19 23:27:09 -0500

    Se resuelve LongestSubStrWithoutRepeatingCh

---
+++ java-algorithm/blind75/src/main/java/twoPointers/LongestSubStrWithoutRepeatingCh.java
@@ -0,0 +1,24 @@
+package twoPointers;
+
+import java.util.HashMap;
+
+public class LongestSubStrWithoutRepeatingCh {
+    public static int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+        int l = 0, res = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            if (mp.containsKey(s.charAt(r))) {
+                l = Math.max(mp.get(s.charAt(r)) + 1, l);
+            }
+            mp.put(s.charAt(r), r);
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+
+    public static void main(String [] args){
+        String s = "zxyzxyz";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+}

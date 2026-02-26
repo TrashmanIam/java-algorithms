1a574f00222e726db6d2836d7ec35e64c6d7c331
Trashman <jeanrevellon.dev@gmail.com>
2026-02-26 16:45:25 -0500

    Correccion del incremento en los pivotes l & r de  SearchInRotatedSortedArray

--- java-algorithm/blind75/src/main/java/binarySearch/SearchInRotatedSortedArray.java
+++ java-algorithm/blind75/src/main/java/binarySearch/SearchInRotatedSortedArray.java
@@ -3,26 +3,31 @@ package binarySearch;
 public class SearchInRotatedSortedArray {
     public static int search(int[] nums, int target) {
 
-        int l = 0, r = nums.length - 1;
+        int l = 0;
+        int r = nums.length - 1;
 
-        while (l <= r){
-            int m = (l + r) / 2;
+        while(l <= r) {
 
-            if (nums[m] == target) return m;
+            int mid = (l + r) / 2;
 
-            if (nums[l] <= nums[m]) {
-                if (target > nums[m] || target < nums[l]){
-                    l = m + 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[l] <= nums[mid]) {
+                if (target > nums[mid] || target < nums[l]) {
+                    l = mid + 1;
                 } else {
-                    r = m - 1;
+                    r = mid - 1;
                 }
             } else {
-                if (target < nums[m] || target > nums[r]){
-                    r = m - 1;
+                if (target < nums[mid] || target > nums [r]) {
+                    r = mid - 1;
                 } else {
-                    r = m + 1;
+                    l = mid + 1;
                 }
             }
+
         }
 
         return -1;
